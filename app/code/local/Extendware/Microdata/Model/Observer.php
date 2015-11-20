4acf1e5e7cc66fbf0da920d41d50310891d8904a
manish <manish>
2015-11-20 18:43:16 +0530

    Done Some Basic Setup For HTML Overwrite

--- app/code/local/Extendware/Microdata/Model/Observer.php
+++ app/code/local/Extendware/Microdata/Model/Observer.php
@@ -6,24 +6,24 @@ class Extendware_Microdata_Model_Observer extends Varien_Object {
 
     public function coreBlockAbstractToHtmlAfter(Varien_Event_Observer $observer) {
 
+        $containerselector = Mage::getStoreConfig('microdatasection/micro/containgerselector');
+        $enable = Mage::getStoreConfig('microdatasection/micro/ismandatory');
+        if ($enable==1) {
+            $block = $observer->getBlock();
+            if ($block instanceof Mage_Catalog_Block_Product_View) {
+                $html = $observer->getTransport()->getHtml();
+                $myFormHtml = 'Get your form HTML';
 
+                $html = str_get_html($html);
 
+                foreach ($html->find($containerselector) as $key => $p_tags) {
+                    $p_tags->{'typeof'} = 'Product';
+                    $p_tags->{'vocab'} = 'http://schema.org/';
+                }
 
-        $block = $observer->getBlock();
-        if ($block instanceof Mage_Catalog_Block_Product_View) {
-            $html = $observer->getTransport()->getHtml();
-            $myFormHtml = 'Get your form HTML';
-            
-            $html = str_get_html($html);
-           
-            foreach($html->find('div[class="product-view"]') as $key => $p_tags) {
-                   $p_tags->{'typeof'}= 'Product';
-                $p_tags->{'vocab'} = 'http://schema.org/';
-                    
+                $html = $myFormHtml . $html;
+                $observer->getTransport()->setHtml($html);
             }
-
-            $html = $myFormHtml . $html;
-            $observer->getTransport()->setHtml($html);
         }
     }
 
